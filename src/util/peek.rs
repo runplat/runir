9d56a8120f1fa399c81726157ea7383b0a319561
Julius Liu <juliusl@microsoft.com>
2025-07-01 16:34:08 -0700

    feat: consume the peek reference for extension api

--- src/util/peek.rs
+++ src/util/peek.rs
@@ -21,80 +21,80 @@ impl<'peek> Deref for Peek<'peek> {
 /// Extension methods for working with a Peek container
 pub trait PeekExtensions<'peek> {
     /// Peek at the reader under a specific key, if the current peek context is a map
-    fn at(&'peek self, key: &str) -> Option<Peek<'peek>>;
+    fn at(self, key: &str) -> Option<Peek<'peek>>;
 
     /// Returns a bool if the current peek context is a bool
-    fn bool(&'peek self) -> Option<bool>;
+    fn bool(self) -> Option<bool>;
 
     /// Returns a str if the current peek context is a str
-    fn str(&'peek self) -> Option<&'peek str>;
+    fn str(self) -> Option<&'peek str>;
 
     /// Returns a u64 if the current peek context is a u64
-    fn u64(&'peek self) -> Option<u64>;
+    fn u64(self) -> Option<u64>;
 }
 
 impl<'peek> PeekExtensions<'peek> for Peek<'peek> {
     #[inline]
-    fn bool(&'peek self) -> Option<bool> {
+    fn bool(self) -> Option<bool> {
         self.0.get_bool().ok()
     }
 
     #[inline]
-    fn str(&'peek self) -> Option<&'peek str> {
+    fn str(self) -> Option<&'peek str> {
         self.0.get_str().ok()
     }
 
     #[inline]
-    fn u64(&'peek self) -> Option<u64> {
+    fn u64(self) -> Option<u64> {
         self.0.get_u64().ok()
     }
 
     #[inline]
-    fn at(&'peek self, key: &str) -> Option<Peek<'peek>> {
+    fn at(self, key: &str) -> Option<Peek<'peek>> {
         self.get_map().ok().map(|p| Peek(p.idx(key)))
     }
 }
 
 impl<'peek> PeekExtensions<'peek> for Option<Peek<'peek>> {
     #[inline]
-    fn bool(&'peek self) -> Option<bool> {
-        self.as_ref().and_then(|r| r.bool())
+    fn bool(self) -> Option<bool> {
+        self.and_then(|r| r.bool())
     }
 
     #[inline]
-    fn str(&'peek self) -> Option<&'peek str> {
-        self.as_ref().and_then(|r| r.str())
+    fn str(self) -> Option<&'peek str> {
+        self.and_then(|r| r.str())
     }
 
     #[inline]
-    fn u64(&'peek self) -> Option<u64> {
-        self.as_ref().and_then(|r| r.u64())
+    fn u64(self) -> Option<u64> {
+        self.and_then(|r| r.u64())
     }
 
     #[inline]
-    fn at(&'peek self, key: &str) -> Option<Peek<'peek>> {
-        self.as_ref().and_then(|r| r.at(key))
+    fn at(self, key: &str) -> Option<Peek<'peek>> {
+        self.and_then(|r| r.at(key))
     }
 }
 
 impl<'peek> PeekExtensions<'peek> for Option<&'peek Peek<'peek>> {
     #[inline]
-    fn bool(&'peek self) -> Option<bool> {
-        self.as_ref().and_then(|r| r.bool())
+    fn bool(self) -> Option<bool> {
+        self.cloned().and_then(|r| r.bool())
     }
 
     #[inline]
-    fn str(&'peek self) -> Option<&'peek str> {
-        self.as_ref().and_then(|r| r.str())
+    fn str(self) -> Option<&'peek str> {
+        self.cloned().and_then(|r| r.str())
     }
 
     #[inline]
-    fn u64(&'peek self) -> Option<u64> {
-        self.as_ref().and_then(|r| r.u64())
+    fn u64(self) -> Option<u64> {
+        self.cloned().and_then(|r| r.u64())
     }
 
     #[inline]
-    fn at(&'peek self, key: &str) -> Option<Peek<'peek>> {
-        self.as_ref().and_then(|r| r.at(key))
+    fn at(self, key: &str) -> Option<Peek<'peek>> {
+        self.cloned().and_then(|r| r.at(key))
     }
 }
