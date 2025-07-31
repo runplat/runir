5a8d10b6f4590e63c4f69bd251aaf9f337bde339
Julius Liu <juliusl@microsoft.com>
2025-07-30 18:44:07 -0700

    nit: docs/inline annotations

--- src/util/run.rs
+++ src/util/run.rs
@@ -7,19 +7,19 @@ use super::PeekExtensions;
 use crate::{IRecord, vol::Volume};
 use serde::de::DeserializeOwned;
 
-/// Wraps an IRecord to provide an IRecord interface w/ a mutable interface w/ the stored object
+/// Wraps an IRecord type and provides copy-on-write semantics for the stored object
 pub struct RunCell<T, R>(Volume<OnceLock<T>, R>);
 
 impl<T, R: IRecord> RunCell<T, R> {
-    /// Creates a new run cell w/ source
+    /// Creates a new run cell w/ source record
     #[inline]
     pub fn new(record: R) -> Self {
         Self((OnceLock::new(), record).into())
     }
 
-    /// Resets the inner state
+    /// Resets the mutated state
     ///
-    /// Returns the existing value of T if a value was set
+    /// Returns the previously mutated state, or None if no mutations occured
     #[inline]
     pub fn reset(&mut self) -> Option<T> {
         self.0.target_mut().take()
@@ -39,58 +39,71 @@ impl<T, R: IRecord> RunCell<T, R> {
 }
 
 impl<T, R: IRecord> IRecord for RunCell<T, R> {
+    #[inline]
     fn ns_chk(&self) -> u64 {
         self.0.encoder().ns_chk()
     }
 
+    #[inline]
     fn uuid(&self) -> uuid::Uuid {
         self.0.encoder().uuid()
     }
 
+    #[inline]
     fn opts(&self) -> &crate::Opts {
         self.0.encoder().opts()
     }
 
+    #[inline]
     fn opts_mut(&mut self) -> Option<&mut crate::Opts> {
         self.0.encoder_mut().opts_mut()
     }
 
+    #[inline]
     fn bytes(&self) -> &[u8] {
         self.0.encoder().bytes()
     }
 
+    #[inline]
     fn to_record(&self) -> crate::Record {
         self.0.encoder().to_record()
     }
 }
 
 impl<'b, T, R: IRecord> IRecord for &'b RunCell<T, R> {
+    #[inline]
     fn ns_chk(&self) -> u64 {
         <RunCell<T, R> as IRecord>::ns_chk(self)
     }
 
+    #[inline]
     fn uuid(&self) -> uuid::Uuid {
         <RunCell<T, R> as IRecord>::uuid(self)
     }
 
+    #[inline]
     fn opts(&self) -> &crate::Opts {
         <RunCell<T, R> as IRecord>::opts(self)
     }
 
+    #[inline]
     fn opts_mut(&mut self) -> Option<&mut crate::Opts> {
         None
     }
 
+    #[inline]
     fn bytes(&self) -> &[u8] {
         <RunCell<T, R> as IRecord>::bytes(self)
     }
 
+    #[inline]
     fn to_record(&self) -> crate::Record {
         <RunCell<T, R> as IRecord>::to_record(self)
     }
 }
 
 impl<T: DeserializeOwned + Default, R: IRecord> AsMut<T> for RunCell<T, R> {
+    #[inline]
     fn as_mut(&mut self) -> &mut T {
         self.as_ref();
 
@@ -101,6 +114,7 @@ impl<T: DeserializeOwned + Default, R: IRecord> AsMut<T> for RunCell<T, R> {
 }
 
 impl<T: DeserializeOwned + Default, R: IRecord> AsRef<T> for RunCell<T, R> {
+    #[inline]
     fn as_ref(&self) -> &T {
         self.0.target()
             .get_or_init(|| self.0.encoder().peek().to_obj::<T>().unwrap_or_default())
@@ -110,18 +124,21 @@ impl<T: DeserializeOwned + Default, R: IRecord> AsRef<T> for RunCell<T, R> {
 impl<T: DeserializeOwned + Default, R: IRecord> Deref for RunCell<T, R> {
     type Target = T;
 
+    #[inline]
     fn deref(&self) -> &Self::Target {
         self.as_ref()
     }
 }
 
 impl<T: DeserializeOwned + Default, R: IRecord> DerefMut for RunCell<T, R> {
+    #[inline]
     fn deref_mut(&mut self) -> &mut Self::Target {
         self.as_mut()
     }
 }
 
 impl<T, R: Clone> Clone for RunCell<T, R> {
+    #[inline]
     fn clone(&self) -> Self {
         Self((OnceLock::new(), self.0.encoder().clone()).into())
     }
