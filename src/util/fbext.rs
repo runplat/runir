e301b78c3aa8c3b550fa3b8c3232ee10d4c27344
Julius Liu <juliusl@microsoft.com>
2025-11-20 18:19:47 -0800

    chore: add object_reader type, re-org, DRY

--- src/util/fbext.rs
+++ src/util/fbext.rs
@@ -1,9 +1,13 @@
-use crate::util::annotate::Annotate;
+use crate::{util::annotate::Annotate, wire::ContentAddress};
+
+use std::{marker::PhantomData, ops::Deref};
+
 use anyhow::anyhow;
 use flexbuffers::{Blob, Buffer};
-use serde::Serialize;
+use serde::{Deserialize, Serialize};
 use sha2::Digest;
 use time::UtcDateTime;
+use zstd::zstd_safe::WriteBuf;
 
 /// Trait for embedding an object into a receiver
 pub trait Object {
@@ -21,6 +25,13 @@ pub trait PushObject<'a> {
 pub trait GetObject<B> {
     /// Enables pushing an object into a builder as an annotated blob
     fn get_object<'a, T: Annotate + 'a>(&self) -> crate::Result<flexbuffers::Reader<B>>;
+}
+
+pub trait AsObject<B> {
+    /// Returns the current reader as a flexbuffer::Reader over an Object root
+    ///
+    /// Returns None if the type_name and/or version have incorrect values
+    fn as_object<T: Annotate>(&self) -> Option<ObjectReader<B, T>>;
 }
 
 impl<'a> Object for flexbuffers::MapBuilder<'a> {
@@ -71,8 +82,27 @@ where
     B::BufferString: AsRef<str>,
 {
     fn get_object<'a, T: Annotate + 'a>(&self) -> crate::Result<flexbuffers::Reader<B>> {
-        if let Some(type_name) = self
-            .as_map()
+        if let Some(object) = self.as_object::<T>() {
+            let span = tracing::trace_span!(
+                "get_object",
+                type_name = object.type_name().as_ref(),
+                size = object.size(),
+                created = object.created().unix_timestamp(),
+            );
+            span.in_scope(|| object.read())
+        } else {
+            Err(anyhow!("Does not have version set").into())
+        }
+    }
+}
+
+impl<B> AsObject<B> for flexbuffers::Reader<B>
+where
+    B: Buffer,
+    B::BufferString: AsRef<str>,
+{
+    fn as_object<T: Annotate>(&self) -> Option<ObjectReader<B, T>> {
+        self.as_map()
             .idx("version")
             .get_str()
             .ok()
@@ -84,25 +114,92 @@ where
                     .ok()
                     .filter(|t| t.as_ref() == T::type_name()),
             )
-        {
-            let span = tracing::trace_span!(
-                "get_object",
-                type_name = type_name.as_ref(),
-                size = self.as_map().idx("size").as_u64(),
-                created = self.as_map().idx("ts").as_u64()
-            );
-            span.in_scope(|| {
-                if let Some(Blob(object)) = self.as_map().idx("object").get_blob().ok() {
-                    Ok(flexbuffers::Reader::get_root(object)?)
-                } else if let Some(error) = self.as_map().idx("error").get_str().ok() {
-                    Err(anyhow!(error.to_string()).into())
-                } else {
-                    Err(anyhow!("Invalid object map").into())
-                }
+            .map(|_| ObjectReader {
+                reader: self.clone(),
+                _t: Default::default(),
             })
+    }
+}
+
+/// Wrapper returned after validating the stored type
+pub struct ObjectReader<B, T> {
+    reader: flexbuffers::Reader<B>,
+    _t: PhantomData<T>,
+}
+
+impl<B, T> ObjectReader<B, T>
+where
+    B: flexbuffers::Buffer,
+    B::BufferString: AsRef<str>,
+{
+    /// Returns the value of the version field
+    #[inline]
+    pub fn version(&self) -> impl AsRef<str> {
+        self.idx("version").as_str()
+    }
+
+    /// Returns the type name of the stored object
+    #[inline]
+    pub fn type_name(&self) -> impl AsRef<str> {
+        self.idx("type_name").as_str()
+    }
+
+    /// Returns the size of the object's flexbuffer root
+    #[inline]
+    pub fn size(&self) -> u64 {
+        self.idx("ts").as_u64()
+    }
+
+    /// Returns the timestamp this object was created
+    #[inline]
+    pub fn created(&self) -> UtcDateTime {
+        UtcDateTime::from_unix_timestamp(self.idx("ts").as_i64()).unwrap_or(UtcDateTime::UNIX_EPOCH)
+    }
+
+    /// Reads the flexbuffer root of this object
+    ///
+    /// Returns an error if the object map is invalid
+    #[inline]
+    pub fn read(&self) -> crate::Result<flexbuffers::Reader<B>> {
+        if let Some(Blob(object)) = self.idx("object").get_blob().ok() {
+            Ok(flexbuffers::Reader::get_root(object)?)
+        } else if let Some(error) = self.idx("error").get_str().ok() {
+            Err(anyhow!(error.to_string()).into())
         } else {
-            Err(anyhow!("Does not have version set").into())
+            Err(anyhow!("Invalid object map").into())
         }
+    }
+
+    /// Returns Some(valid) if sha256 CAS is enabled
+    /// 
+    /// Otherwise; returns None
+    #[inline]
+    pub fn check_sha256(&self, matches: ContentAddress) -> Option<bool> {
+        self.idx("sha256").get_blob().ok().map(|b| {
+            b.0.as_slice() == matches.digest
+        })
+    }
+
+    fn idx(&self, key: &str) -> flexbuffers::Reader<B> {
+        self.as_map().idx(key)
+    }
+}
+
+impl<'a, T: Deserialize<'a>> ObjectReader<&'a [u8], T> {
+    /// Deserializes the stored object that borrows from the underlying buffer
+    ///
+    /// Returns an error if the object mapped is invalid or if deserialization failed
+    #[inline]
+    pub fn to_object(self) -> crate::Result<T> {
+        Ok(flexbuffers::from_slice(self.read()?.buffer())?)
+    }
+}
+
+impl<B, T> Deref for ObjectReader<B, T> {
+    type Target = flexbuffers::Reader<B>;
+
+    fn deref(&self) -> &Self::Target {
+        &self.reader
     }
 }
 
@@ -123,7 +220,7 @@ mod tests {
     }
 
     #[test]
-    fn test_object_exts() {
+    fn test_flexbuffer_exts() {
         let test = Test {
             value: "hello world",
         };
@@ -151,6 +248,9 @@ mod tests {
                 .idx("value")
                 .as_str(),
             "hello world"
-        )
+        );
+
+        let test = object.as_object::<Test>().unwrap().to_object().unwrap();
+        assert_eq!(test.value, "hello world");
     }
 }
